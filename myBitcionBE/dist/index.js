1c95f17b0dd9b07f057fa6b88b1255e583373db8
coinx42-glitch <coinx42@gmail.com>
2024-09-04 19:02:16 +0100

    Update index.js

--- myBitcionBE/dist/index.js
+++ myBitcionBE/dist/index.js
@@ -28,6 +28,7 @@ const corsOptions = {
 };
 app.use((0, cors_1.default)(corsOptions));
 app.use(express_1.default.json());
+app.set('trust proxy', 1);
 app.use((0, morgan_1.default)("dev"));
 app.use((0, express_session_1.default)({
     secret: process.env.SESSION_SECRET,

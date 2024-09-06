aecec1fd5283847d8d32476bf36e2f115a82db96
coinx42-glitch <coinx42@gmail.com>
2024-09-06 18:45:32 +0100

    Update App.tsx

--- myBitcion/src/App.tsx
+++ myBitcion/src/App.tsx
@@ -2,6 +2,9 @@
 import RouterScreen from "./router/RouterScreen";
 
 const App = () => {
+		useEffect(() => {
+		console.countReset(data)
+	}, [data])
 	return (
 		<div className="font-sans">
 			<RouterScreen />
